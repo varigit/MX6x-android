fc1e7c98036d18c6c5ed8bde23a66ba62557dcce
Pierluigi Passaro <pierluigi.p@variscite.com>
2018-03-16 23:46:59 +0100

    update build id and date

--- var_mx6/build_id.mk
+++ var_mx6/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.0-ga
-export BUILD_NUMBER=20170427
+export BUILD_ID=2.0.0-ga
+export BUILD_NUMBER=20180316
