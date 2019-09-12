aaaa3a945941d63ff8f5b77d59f038f33d880f6e
Pierluigi Passaro <pierluigi.p@variscite.com>
2019-09-12 18:51:23 +0200

    imx8qm_var_som: Add HDMI and DP support

--- imx8q/som_mx8q/som_mx8qm.mk
+++ imx8q/som_mx8q/som_mx8qm.mk
@@ -14,10 +14,12 @@ PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/init.imx8qm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.freescale.imx8qm.rc \
     $(IMX_DEVICE_PATH)/init.brcm.wifibt.8qm.sh:vendor/bin/init.brcm.wifibt.sh
 
-BOARD_PREBUILT_DTBOIMAGE := out/target/product/som_mx8q/dtbo-imx8qm-var-som.img
+BOARD_PREBUILT_DTBOIMAGE := out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img
 
 TARGET_BOARD_DTS_CONFIG := \
-        imx8qm-var-som:fsl-imx8qm-var-som.dtb
+        imx8qm-var-som-dp:fsl-imx8qm-var-som-dp.dtb \
+        imx8qm-var-som-hdmi:fsl-imx8qm-var-som-hdmi.dtb \
+        imx8qm-var-som-lvds:fsl-imx8qm-var-som-lvds.dtb
 
 TARGET_BOOTLOADER_CONFIG := \
         imx8qm:imx8qm_var_som_android_defconfig \
