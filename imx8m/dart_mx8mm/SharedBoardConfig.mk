e18e0796f3af33a0fbce7de2308af8aac9b3c10c
Mohit Singh <pal.mohit.singh@gmail.com>
2020-03-30 16:54:37 -0700

    imx8mm_var_som: inital commit to support Android 10.
      - Files added from Android 9 to Android 10.
      - Part 3 of 4.

---
+++ imx8m/dart_mx8mm/SharedBoardConfig.mk
@@ -0,0 +1,14 @@
+KERNEL_NAME := Image
+TARGET_KERNEL_ARCH := arm64
+
+#Enable this to include trusty support
+PRODUCT_IMX_TRUSTY := true
+
+#Enable this to disable product partition build.
+#IMX_NO_PRODUCT_PARTITION := true
+
+# BCM fmac wifi driver module
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_OUT)/drivers/net/ethernet/freescale/fec.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko
---
+++ imx8m/dart_mx8mm/UbootKernelBoardConfig.mk
@@ -0,0 +1,17 @@
+TARGET_BOOTLOADER_POSTFIX := bin
+UBOOT_POST_PROCESS := true
+
+ TARGET_BOOTLOADER_CONFIG := \
+         imx8mm-var-dart:imx8mm_var_dart_android_defconfig \
+         imx8mm-var-dart-uuu:imx8mm_var_dart_android_uuu_defconfig
+
+# imx8mm kernel defconfig
+TARGET_KERNEL_DEFCONFIG := imx8_var_android_defconfig
+#TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
+
+# absolute path is used, not the same as relative path used in AOSP make
+TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
