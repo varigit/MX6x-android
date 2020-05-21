98a9e9fcde4eeb559d3b7578627a4703c2ab3d8b
Mohit Singh <mohit.p@variscite.com>
2020-05-21 13:15:56 -0700

    imx8qm_var_som: Changes to fix wifi suspend/resume crash.

--- imx8q/som_mx8q/som_mx8qm.mk
+++ imx8q/som_mx8q/som_mx8qm.mk
@@ -15,8 +15,7 @@ PRODUCT_COPY_FILES += \
     $(FSL_PROPRIETARY_PATH)/linux-firmware-imx/firmware/hdmi/cadence/hdmitxfw.bin:vendor/firmware/hdp/hdmitxfw.bin \
     $(FSL_PROPRIETARY_PATH)/linux-firmware-imx/firmware/hdmi/cadence/hdmirxfw.bin:vendor/firmware/hdp/hdmirxfw.bin \
     $(IMX_DEVICE_PATH)/init.imx8qm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.freescale.imx8qm.rc \
-    $(IMX_DEVICE_PATH)/ueventd.freescale.8qm.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-    $(IMX_DEVICE_PATH)/init.brcm.wifibt.8qm.sh:vendor/bin/init.brcm.wifibt.sh
+    $(IMX_DEVICE_PATH)/ueventd.freescale.8qm.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 BOARD_PREBUILT_DTBOIMAGE := out/target/product/som_mx8q/dtbo-imx8qm-var-som-lvds.img
 
@@ -32,3 +31,6 @@ TARGET_BOOTLOADER_CONFIG := \
         imx8qm:imx8qm_var_som_android_defconfig \
         imx8qm-som-uuu:imx8qm_var_som_android_uuu_defconfig
 
+#WiFI BT script
+PRODUCT_PACKAGES += \
+    init.brcm.wifibt.8qm.sh
