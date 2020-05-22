2507475a7a7823e4bacdb6773b809a448891d0f0
Mohit Singh <mohit.p@variscite.com>
2020-05-21 17:24:44 -0700

    imx8qxp_var_som: Fix for suspend/resume crash.

--- imx8q/som_mx8q/wifi-bt/Android.mk
+++ imx8q/som_mx8q/wifi-bt/Android.mk
@@ -14,3 +14,18 @@ LOCAL_POST_INSTALL_CMD := mkdir -p $(TARGET_OUT_VENDOR)/bin; \
     ln -sf init.brcm.wifibt.sh init.brcm.wifibt.resume.sh;
 
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := init.brcm.wifibt.8qxp.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/bin
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := SCRIPTS
+LOCAL_SRC_FILES:= ../init.brcm.wifibt.8qxp.sh
+LOCAL_POST_INSTALL_CMD := mkdir -p $(TARGET_OUT_VENDOR)/bin; \
+    cd $(TARGET_OUT_VENDOR)/bin; \
+    mv init.brcm.wifibt.8qxp.sh init.brcm.wifibt.sh; \
+    ln -sf init.brcm.wifibt.sh init.brcm.wifibt.suspend.sh; \
+    ln -sf init.brcm.wifibt.sh init.brcm.wifibt.resume.sh;
+
+include $(BUILD_PREBUILT)
