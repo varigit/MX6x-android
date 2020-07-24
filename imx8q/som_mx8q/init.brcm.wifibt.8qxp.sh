db5b17667c35c145f12016b218848c91c60032c5
Mohit Singh <mohit.p@variscite.com>
2020-07-24 11:15:06 -0700

    imx8qxp_var_som: remove EEPROM read from wifi script.

--- imx8q/som_mx8q/init.brcm.wifibt.8qxp.sh
+++ imx8q/som_mx8q/init.brcm.wifibt.8qxp.sh
@@ -121,15 +121,8 @@ wifi_down()
 # Return true if SOM has WIFI module assembled
 wifi_is_available()
 {
-	# Read SOM options EEPROM field
-	opt=$(/system/bin/i2cget -f -y 0x0 0x52 0x20)
-
-	# Check WIFI bit in SOM options
-	if [ $((opt & 0x1)) -eq 1 ]; then
-		return 0
-	else
-		return 1
-	fi
+	# For now assume WIFI is always available
+	return 0
 }
 
 # Return true if WIFI should not be started
