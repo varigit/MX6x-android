024368df442d462244c6fdede24781628c9da563
Harshesh Valera <harshesh.v@variscite.com>
2021-06-04 21:26:40 -0700

    imx8mn_var_som: Fix BT for 2G / 5G soms
    Use ro.boot.som_imx8mn and ro.boot.bt_firmware to override firmware
    
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- imx8m/som_mx8mn/init.brcm.wifibt.sh
+++ imx8m/som_mx8mn/init.brcm.wifibt.sh
@@ -100,6 +100,8 @@ wifi_up()
 	
 	# Load WIFI driver
 	modprobe -d /vendor/lib/modules brcmfmac p2pon=1
+
+
 }
 
 # Power down WIFI chip
@@ -242,6 +244,14 @@ wifi_start
 # BT_BUF up
 echo 0 > /sys/class/gpio/gpio${BT_BUF_GPIO}/value
 
+chip_id=`cat /sys/bus/mmc/devices/mmc0\:0001/mmc0\:0001\:1/device`
+
+setprop ro.boot.som_imx8mn "true"
+if [ "$chip_id" = "0x4339" ]; then
+        setprop ro.boot.bt_firmware "BCM4335C0.hcd"
+else
+        setprop ro.boot.bt_firmware "BCM43430A1.hcd"
+fi
 # always set property even if wifi failed
 # as property value "1" is expected in early-boot trigger
 setprop sys.brcm.wifibt.completed 1
