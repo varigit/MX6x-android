c65e953e8062e252687c35a4a3aabf0068ba4df7
Natalia Kovalenko <natasha.k@variscite.com>
2025-09-23 16:49:06 +0200

    imx8mn-var-som: Copy default MCU demo image to target product directory
    Copy default MCU demo image to target product directory ${imagesdir}.
    This fixes issue when running command var-mksdcard.sh:
    cp: cannot stat 'out/target/product/som_mx8mn/cm_rpmsg_lite_pingpong_rtos_linux_remote.bin.debug': No such file or directory
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

--- imx8m/som_mx8mn/som_mx8mn.mk
+++ imx8m/som_mx8mn/som_mx8mn.mk
@@ -254,7 +254,8 @@ PRODUCT_COPY_FILES += \
 	device/variscite/imx8m/som_mx8mn/cm_hello_world.bin.debug:vendor/firmware/cm_hello_world.bin.debug \
 	device/variscite/imx8m/som_mx8mn/cm_hello_world.elf.ddr_debug:vendor/firmware/cm_hello_world.elf.ddr_debug \
 	device/variscite/imx8m/som_mx8mn/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.debug:vendor/firmware/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.debug \
-	device/variscite/imx8m/som_mx8mn/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.ddr_debug:vendor/firmware/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.ddr_debug
+	device/variscite/imx8m/som_mx8mn/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.ddr_debug:vendor/firmware/cm_rpmsg_lite_str_echo_rtos_imxcm7.elf.ddr_debug \
+	device/variscite/imx8m/som_mx8mn/cm_rpmsg_lite_pingpong_rtos_linux_remote.bin.debug:cm_rpmsg_lite_pingpong_rtos_linux_remote.bin.debug
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.typec.legacy=true \
