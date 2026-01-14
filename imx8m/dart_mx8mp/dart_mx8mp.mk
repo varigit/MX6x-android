79581033a1b52d834eb631e5dedc1402b0ffae06
Pitchaiah P <pitchaiah.p@variscite.com>
2026-01-14 21:06:50 +0100

    imx8mp-var-dart: Remove custom Wi-Fi config and use NXP default
    Delete dart_mx8mp/wifi_mod_para_sd8987.conf (custom configuration)
    Update PRODUCT_COPY_FILES to point to vendor/nxp/imx-firmware/android_wifi_mod_para.conf
    for default SD8987 Wi-Fi firmware configuration.
    
    Signed-off-by: Pitchaiah P <pitchaiah.p@variscite.com>

--- imx8m/dart_mx8mp/dart_mx8mp.mk
+++ imx8m/dart_mx8mp/dart_mx8mp.mk
@@ -428,7 +428,7 @@ PRODUCT_COPY_FILES += \
     vendor/nxp/imx-firmware/nxp/FwImage_IW612_SD/uartspi_n61x_v1.bin.se:vendor/firmware/nxp/uartspi_n61x_v1.bin.se \
     vendor/nxp/imx-firmware/nxp/FwImage_IW612_SD/sd_w61x_v1.bin.se:vendor/firmware/sd_w61x_v1.bin.se \
     vendor/nxp/imx-firmware/nxp/mfguart/helper_uart_3000000.bin:vendor/firmware/helper_uart_3000000.bin \
-    device/variscite/imx8m/dart_mx8mp/wifi_mod_para_sd8987.conf:vendor/firmware/wifi_mod_para_sd8987.conf
+    vendor/nxp/imx-firmware/nxp/android_wifi_mod_para.conf:vendor/firmware/wifi_mod_para_sd8987.conf
 
 # Sterling LWB / LWB5 wifi and bluetooth combo Firmware
 PRODUCT_COPY_FILES += \
