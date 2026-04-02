ff7fa0291c436461225ce37d276d9319da69d672
Marius Bagassien <marius.b@variscite.com>
2026-04-02 21:16:15 +0200

    som_mx8q: Add missing UUU .lst flash scripts for QM m4 and QX SD variants
    Add missing QM m4 .lst files (symphony, symphony 1.x, and spear
    variants with Cortex-M4 DTBOs) and missing QX SD .lst files (b0/c0
    revisions for symphony and symphony 1.x carrier boards with SD). Update
    PRODUCT_COPY_FILES in som_mx8qm.mk and som_mx8qx.mk to include all new
    scripts.
    
    Signed-off-by: Marius Bagassien <marius.b@variscite.com>

--- imx8q/som_mx8q/som_mx8qm.mk
+++ imx8q/som_mx8q/som_mx8qm.mk
@@ -21,14 +21,23 @@ PRODUCT_COPY_FILES += \
 # Variscite UUU eMMC burning scripts
 PRODUCT_COPY_FILES += \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_hdmi.lst:emmc_burn_android_imx8qm_var_som_symphony_hdmi.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_hdmi_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_hdmi_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_lvds.lst:emmc_burn_android_imx8qm_var_som_symphony_lvds.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_lvds_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_lvds_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_dp.lst:emmc_burn_android_imx8qm_var_som_symphony_dp.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_dp_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_dp_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_hdmi.lst:emmc_burn_android_imx8qm_spear_hdmi.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_hdmi_m4.lst:emmc_burn_android_imx8qm_spear_hdmi_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_lvds.lst:emmc_burn_android_imx8qm_spear_lvds.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_lvds_m4.lst:emmc_burn_android_imx8qm_spear_lvds_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_dp.lst:emmc_burn_android_imx8qm_spear_dp.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_spear_dp_m4.lst:emmc_burn_android_imx8qm_spear_dp_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_hdmi.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_hdmi.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_hdmi_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_hdmi_m4.lst \
     device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_lvds.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_lvds.lst \
-    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_dp.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_dp.lst
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_lvds_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_lvds_m4.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_dp.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_dp.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx8qm_var_som_symphony_1.x_dp_m4.lst:emmc_burn_android_imx8qm_var_som_symphony_1.x_dp_m4.lst
 
 # -------@block_cm_rpmsg-------
 # QM has two Cortex-M4 cores (m40 + m41); m40 is in som_mx8q_common.mk
@@ -36,7 +45,7 @@ PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/cm_rpmsg_lite_pingpong_rtos_linux_remote_m41.bin.debug:cm_rpmsg_lite_pingpong_rtos_linux_remote_m41.bin.debug \
     $(IMX_DEVICE_PATH)/cm_rpmsg_lite_pingpong_rtos_linux_remote_m41.elf.debug:$(TARGET_COPY_OUT_VENDOR)/firmware/cm_rpmsg_lite_pingpong_rtos_linux_remote_m41.elf.debug
 
-BOARD_PREBUILT_DTBOIMAGE := $(OUT_DIR)/target/product/som_mx8q/dtbo-imx8qm-var-som-symphony-lvds.img
+BOARD_PREBUILT_DTBOIMAGE := $(OUT_DIR)/target/product/som_mx8q/dtbo-imx8qm-var-som-symphony-lvds-m4.img
 
 # imx8qm Symphony
 TARGET_BOARD_DTS_CONFIG := \
