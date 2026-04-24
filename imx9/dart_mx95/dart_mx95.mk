24fbb08616bf4eae60d38e14403565ae5890167d
Natalia Kovalenko <natasha.k@variscite.com>
2026-04-24 14:27:29 +0200

    uuu_scripts: imx95: Add the UUU command list files with CM7 demo image
    Add the UUU command list reference files to burn image with Cortex M7
    support enabled.
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

--- imx9/dart_mx95/dart_mx95.mk
+++ imx9/dart_mx95/dart_mx95.mk
@@ -163,7 +163,9 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_REPO_PATH)/common/tools/fastboot_imx_flashall.sh:fastboot_imx_flashall.sh \
     $(CONFIG_REPO_PATH)/common/tools/imx-sdcard-partition.sh:imx-sdcard-partition.sh \
     $(CONFIG_REPO_PATH)/common/tools/uuu_imx_android_flash.bat:uuu_imx_android_flash.bat \
-    $(CONFIG_REPO_PATH)/common/tools/uuu_imx_android_flash.sh:uuu_imx_android_flash.sh
+    $(CONFIG_REPO_PATH)/common/tools/uuu_imx_android_flash.sh:uuu_imx_android_flash.sh \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx95_var_dart_dt8mcustomboard_m7.lst:emmc_burn_android_imx95_var_dart_dt8mcustomboard_m7.lst \
+    device/variscite/scripts/uuu_scripts/emmc_burn_android_imx95_var_dart_sonata_m7.lst:emmc_burn_android_imx95_var_dart_sonata_m7.lst
 
 # -------@block_security-------
 
