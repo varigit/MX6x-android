24d0c15d6d630aa02d0fe9d1e13a934d78c2637d
Alifer Moraes <alifer.m@variscite.com>
2026-07-03 14:58:10 -0300

    som_mx8mn: imx-mkimage: use varigit fork
    Variscite carries several downstream imx-mkimage changes to support its
    SoM requirements. These changes were previously applied as local patches
    on top of the NXP imx-mkimage repository, but maintaining the growing
    patch set in the layer made updates harder and complicated manual
    imx-boot builds.
    
    Use the Variscite imx-mkimage repository and branch instead, where the
    required changes are already applied.
    
    The imx-mkimage branch from varigit was checkout out from:
    nxp-imx/imx-mkimage@9e60b1f7a87a
    
    The followings patches were applied:
    varigit/imx-mkimage@58f79e33fb27 iMX8QX: soc: Save boot-spl-container.img in flash_spl target
    varigit/imx-mkimage@a236c415b18e imx8mm: Add dual bootloader support for imx8mm.
    varigit/imx-mkimage@3315bbd90208 iMX8M: soc: imx8mm: move TEE_LOAD_ADDR to 512mb memory region
    varigit/imx-mkimage@7b9fa136959e iMX8M: soc: add variscite imx8mm support
    varigit/imx-mkimage@fa17dda82480 iMX8M: soc: change padding of DDR4 and LPDDR4 DMEM firmware
    varigit/imx-mkimage@cf71fb067f1b iMX8M: soc: allow dtb override
    
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- imx8m/som_mx8mn/AndroidUboot.sh
+++ imx8m/som_mx8mn/AndroidUboot.sh
@@ -38,41 +38,41 @@ build_pre_image()
 build_imx_uboot()
 {
 	echo Building i.MX U-Boot with firmware
-	cp ${UBOOT_OUT}/u-boot-nodtb.$1 ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
-	cp ${UBOOT_OUT}/spl/u-boot-spl.bin  ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
-	cp ${UBOOT_OUT}/tools/mkimage  ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/mkimage_uboot
-	cp ${UBOOT_OUT}/arch/arm/dts/imx8mn-var-som-symphony.dtb ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
-	cp ${FSL_PROPRIETARY_PATH}/linux-firmware-imx/firmware/ddr/synopsys/ddr4* ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/
+	cp ${UBOOT_OUT}/u-boot-nodtb.$1 ${VARISCITE_PATH}/imx-mkimage/iMX8M/
+	cp ${UBOOT_OUT}/spl/u-boot-spl.bin  ${VARISCITE_PATH}/imx-mkimage/iMX8M/
+	cp ${UBOOT_OUT}/tools/mkimage  ${VARISCITE_PATH}/imx-mkimage/iMX8M/mkimage_uboot
+	cp ${UBOOT_OUT}/arch/arm/dts/imx8mn-var-som-symphony.dtb ${VARISCITE_PATH}/imx-mkimage/iMX8M/
+	cp ${FSL_PROPRIETARY_PATH}/linux-firmware-imx/firmware/ddr/synopsys/ddr4* ${VARISCITE_PATH}/imx-mkimage/iMX8M/
 
 	# build ATF based on whether tee is involved
 	make -C ${VARISCITE_PATH}/arm-trusted-firmware/ PLAT=`echo $2 | cut -d '-' -f1` clean
 	if [ `echo $2 | cut -d '-' -f4` = "trusty" ]; then
-		cp -v ${product_path}/tee-imx8mn.bin ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/tee.bin
+		cp -v ${product_path}/tee-imx8mn.bin ${VARISCITE_PATH}/imx-mkimage/iMX8M/tee.bin
 		make -C ${VARISCITE_PATH}/arm-trusted-firmware/ CROSS_COMPILE="${ATF_CROSS_COMPILE}" PLAT=`echo $2 | cut -d '-' -f1` bl31 -B SPD=trusty IMX_ANDROID_BUILD=true 1>/dev/null || exit 1
 	else
-		if [ -f ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/tee.bin ] ; then
-			rm -rf ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/tee.bin
+		if [ -f ${VARISCITE_PATH}/imx-mkimage/iMX8M/tee.bin ] ; then
+			rm -rf ${VARISCITE_PATH}/imx-mkimage/iMX8M/tee.bin
 		fi
-		if [ -f ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/tee.bin.lz4 ] ; then
-			rm -rf ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/tee.bin.lz4
+		if [ -f ${VARISCITE_PATH}/imx-mkimage/iMX8M/tee.bin.lz4 ] ; then
+			rm -rf ${VARISCITE_PATH}/imx-mkimage/iMX8M/tee.bin.lz4
 		fi
 		make -C ${VARISCITE_PATH}/arm-trusted-firmware/ CROSS_COMPILE="${ATF_CROSS_COMPILE}" PLAT=`echo $2 | cut -d '-' -f1` bl31 -B IMX_ANDROID_BUILD=true 1>/dev/null || exit 1
 	fi
-	cp ${VARISCITE_PATH}/arm-trusted-firmware/build/`echo $2 | cut -d '-' -f1`/release/bl31.bin ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/bl31.bin
+	cp ${VARISCITE_PATH}/arm-trusted-firmware/build/`echo $2 | cut -d '-' -f1`/release/bl31.bin ${VARISCITE_PATH}/imx-mkimage/iMX8M/bl31.bin
 
-	make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ clean
+	make -C ${VARISCITE_PATH}/imx-mkimage/ clean
 	if [ `echo $2 | rev | cut -d '-' -f1 | rev` != "dual" ]; then
-		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" flash_ddr4_evk || exit 1
-		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" print_fit_hab_ddr4 || exit 1
+		make -C ${VARISCITE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" flash_ddr4_evk || exit 1
+		make -C ${VARISCITE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" print_fit_hab_ddr4 || exit 1
 	else
-		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" flash_ddr4_evk_no_hdmi_dual_bootloader || exit 1
-		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" PRINT_FIT_HAB_OFFSET=0x0 print_fit_hab_ddr4 || exit 1
+		make -C ${VARISCITE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" flash_ddr4_evk_no_hdmi_dual_bootloader || exit 1
+		make -C ${VARISCITE_PATH}/imx-mkimage/ SOC=iMX8MN dtbs="${UBOOT_DTB}" PRINT_FIT_HAB_OFFSET=0x0 print_fit_hab_ddr4 || exit 1
 	fi
 
 	if [ `echo $2 | rev | cut -d '-' -f1 | rev` != "dual" ]; then
-		cp ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/flash.bin ${UBOOT_COLLECTION}/u-boot-$2.imx
+		cp ${VARISCITE_PATH}/imx-mkimage/iMX8M/flash.bin ${UBOOT_COLLECTION}/u-boot-$2.imx
 	else
-		cp ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/flash.bin ${UBOOT_COLLECTION}/spl-$2.bin
-		cp ${IMX_MKIMAGE_PATH}/imx-mkimage/iMX8M/u-boot-ivt.itb ${UBOOT_COLLECTION}/bootloader-$2.img
+		cp ${VARISCITE_PATH}/imx-mkimage/iMX8M/flash.bin ${UBOOT_COLLECTION}/spl-$2.bin
+		cp ${VARISCITE_PATH}/imx-mkimage/iMX8M/u-boot-ivt.itb ${UBOOT_COLLECTION}/bootloader-$2.img
 	fi
 }
